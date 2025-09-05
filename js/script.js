4eef2f3d75b8f748dfc203d0de9f310e01fc39f9
toni <ahfatoni909@gmail.com>
2025-09-05 21:10:28 +0700

    Final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
+//Hide navbar
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', function() {
-    navMenu.classList.toggle('hidden');
+  hamburger.addEventListener('click', function() {
+      navMenu.classList.toggle('hidden');
+  });
 });
+
+// Ambil semua tombol accordion
+document.addEventListener("DOMContentLoaded", () => {
+  const toggles = document.querySelectorAll(".toggle");
+
+  toggles.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const content = btn.parentElement.nextElementSibling;
+
+      content.classList.toggle("hidden");
+
+      btn.textContent = content.classList.contains("hidden") ? "+" : "−";
+    });
+  });
+});
+
